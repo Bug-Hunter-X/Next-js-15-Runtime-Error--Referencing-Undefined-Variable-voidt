ef8dc614301b56bf030783b4c11056fef7926139
BugHunter-X <askzenai@gmail.com>
2024-12-28 08:34:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,28 @@
+```javascript
+//pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href='/about'>
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+//pages/about.js
+
+export default function About() {
+  //This will cause an error because the variable is not defined in scope
+  console.log(nonExistentVariable);
+  return (
+    <div>
+      <h1>About Page</h1>
+    </div>
+  );
+}
+```
